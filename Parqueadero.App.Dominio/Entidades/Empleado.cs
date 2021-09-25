c516ab2433675dac31eca8ea915fb0ba6f7c3175
josvaldes <josvaldes@gmail.com>
2021-09-24 21:28:47 -0500

    Ajuste a repositorio

--- Parqueadero.App.Dominio/Entidades/Empleado.cs
+++
@@ -1,9 +0,0 @@
-namespace Parqueadero.App.Dominio
-{
-    public class Empleado:DatosPersona
-    {
-        //Asociacion con la clase RolEmpleado
-        public RolEmpleado RolEmpleado { get; set; }
-        
-    }
-}
---
+++ Parqueadero.App/Parqueadero.App.Consola/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Parqueadero.App.Dominio;
+
+namespace Parqueadero.App.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
